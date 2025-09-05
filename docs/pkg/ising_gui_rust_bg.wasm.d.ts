6690ed1f8c3905ca26040680fd5e1b29b5b06b51
Kyle Corbett <kyle.corbett@sjsu.edu>
2025-09-05 16:28:16 -0700

    Fix deployment

---
+++ docs/pkg/ising_gui_rust_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_ising_free: (a: number, b: number) => void;
+export const ising_new: (a: number, b: number, c: number) => number;
+export const ising_step: (a: number) => void;
+export const ising_set_j: (a: number, b: number) => void;
+export const ising_spins_ptr: (a: number) => number;
+export const ising_size: (a: number) => number;
+export const ising_set_temp: (a: number, b: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_start: () => void;
